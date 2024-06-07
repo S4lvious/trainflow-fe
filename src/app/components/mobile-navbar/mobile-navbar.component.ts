62db24b44a59ad67044124b0d3fbfb1b134b9668
Salvatore Liccardo <s.liccardo022@gmail.com>
2024-06-07 15:27:13 +0200

    router link into responsive menu

--- src/app/components/mobile-navbar/mobile-navbar.component.ts
+++ src/app/components/mobile-navbar/mobile-navbar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'tf-mobile-navbar',
     templateUrl: './mobile-navbar.component.html',
     styleUrls: ['./mobile-navbar.component.css'],
-    standalone: true
+    standalone: true,
+    imports: [RouterModule]
 })
 export class MobileNavbarComponent {
     // Add your component logic here
